f86709dfbe13d111d2fbe24edde5097c61e1a655
Manpreet Krishan <manpreet.plus@gmail.com>
2021-09-15 04:13:49 +0530

    ncc

--- dist/index.js
+++ dist/index.js
@@ -7897,19 +7897,19 @@ const format = __nccwpck_require__(8149);
 async function run() {
   try {
     const github_token = core.getInput('GITHUB_TOKEN');
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
 
-    const createdAt = new Date(github.event.pull_request.created_at);
-    const closedAt = new Date(github.event.pull_request.closed_at);
+    const createdAt = new Date(pull_request.created_at);
+    const closedAt = new Date(pull_request.closed_at);
     const timeDifference = closedAt - createdAt;
     const time = formatDistance(0, timeDifference * 1000, { includeSeconds: true });
     const message = `This PR took ${time} to close`;
 
-    const context = github.context;
-
     const octokit = new github.GitHub(github_token);
     const new_comment = octokit.issues.createComment({
         ...context.repo,
-        issue_number: github.event.number,
+        issue_number: pull_request.number,
         body: message
       });
 
